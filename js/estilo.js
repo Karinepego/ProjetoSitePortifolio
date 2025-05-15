f8590a62510d2ab7f7ca0a04540ed0dae3760f6c
Karinepego <karinepegoribeiro@gmail.com>
2025-05-15 01:06:11 -0300

    new and HUGES atualizations

---
+++ js/estilo.js
@@ -0,0 +1,3 @@
+document.querySelector(".contato-btn").addEventListener("click", function() {
+    window.open("https://mail.google.com/mail/?view=cm&fs=1&to=karinepegoribeiro@gmail.com&su=Visitei seu portifólio&body=Olá, visitei seu portifólio e gostaria de trocar uma conversa contigo.", "_blank");
+});

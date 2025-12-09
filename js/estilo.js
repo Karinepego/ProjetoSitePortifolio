169becf85b5f79d6b1c2add83aa6fbad2a5aaa14
Karinepego <karinepegoribeiro@gmail.com>
2025-12-09 16:36:23 -0300

    ajustes na coloração, estruturação de pastas  e informações

--- js/estilo.js
+++ js/estilo.js
@@ -2,6 +2,6 @@ const contatoButtons = document.querySelectorAll(".contato-btn");
 
 contatoButtons.forEach(button => {
     button.addEventListener("click", function() {
-        window.open("https://mail.google.com/mail/?view=cm&fs=1&to=karinepegoribeiro@gmail.com&su=Visitei seu portifólio&body=Olá, visitei seu portifólio e gostaria de trocar uma conversa contigo.", "_blank");
+        window.open("https://mail.google.com/mail/?view=cm&fs=1&to=karinepegoribeiro45@gmail.com&su=Visitei seu portifólio&body=Olá, visitei seu portifólio e gostaria de trocar uma conversa contigo.", "_blank");
     });
 });
